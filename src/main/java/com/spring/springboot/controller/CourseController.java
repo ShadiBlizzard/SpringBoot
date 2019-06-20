02be175a0ab285183e272c7dc3696029f1b42d84
shadiblizzard <giulia.pennati94@gmail.com>
2019-06-20 17:24:14 +0200

    final api

--- src/main/java/com/spring/springboot/controller/CourseController.java
+++ src/main/java/com/spring/springboot/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.spring.springboot.services.CourseService;
 
 @RestController
 @RequestMapping("/courses")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CourseController {
 
 	@Autowired
