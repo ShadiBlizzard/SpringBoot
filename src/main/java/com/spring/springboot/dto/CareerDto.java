b8c899a5c0fb9bfb34880124cc189e7751d90e7a
vinscorson <vinscorson@vinscorsonw.italy.itroot.adnet>
2019-06-27 16:21:43 +0200

    definizione delle classi dto per le entity, implementazione dello student dto

---
+++ src/main/java/com/spring/springboot/dto/CareerDto.java
@@ -0,0 +1,9 @@
+package com.spring.springboot.dto;
+
+import java.io.Serializable;
+
+public class CareerDto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/spring/springboot/dto/ExamsDto.java
@@ -0,0 +1,9 @@
+package com.spring.springboot.dto;
+
+import java.io.Serializable;
+
+public class ExamsDto implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+}
