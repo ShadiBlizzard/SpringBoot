8bc91bb8fadc5a0c5c1a3505379bd098b7ee1091
vinscorson <vinscorson@vinscorsonw.italy.itroot.adnet>
2019-07-01 14:58:17 +0200

    fix student controller

--- src/main/java/com/spring/springboot/controller/StudentController.java
+++ src/main/java/com/spring/springboot/controller/StudentController.java
@@ -8,11 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.springboot.dto.StudentDto;
@@ -29,7 +29,7 @@ public class StudentController {
 	private StudentService studentService;
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<StudentDto> findStudentById(@RequestParam int id) {
+	public ResponseEntity<StudentDto> findStudentById(@PathVariable("id") int id) {
 		StudentDto dto = studentService.findStudentById(id);
 		return new ResponseEntity<>(dto, HttpStatus.FOUND);
 	}
