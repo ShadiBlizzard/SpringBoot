354df53d02c5ebf83a15150a0b0e339734bf2871
vinscorson <vinscorson@vinscorsonw.italy.itroot.adnet>
2019-07-02 16:23:31 +0200

    merge course controll

--- src/main/java/com/spring/springboot/controller/CourseController.java
+++ src/main/java/com/spring/springboot/controller/CourseController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.springboot.dto.CourseDto;
+import com.spring.springboot.exceptions.EmptyListException;
+import com.spring.springboot.exceptions.InvalidOperationException;
+import com.spring.springboot.exceptions.ObjNotFoundException;
 import com.spring.springboot.services.CourseService;
 
 
@@ -28,37 +31,37 @@ public class CourseController {
 	private CourseService courseService;
 	
 	@GetMapping("/name/{name}")
-	public ResponseEntity<CourseDto> findByName(@PathVariable String name) {
+	public ResponseEntity<CourseDto> findByName(@PathVariable String name) throws ObjNotFoundException {
 		CourseDto courseDto = courseService.findByName(name);
 		return new ResponseEntity<>(courseDto, HttpStatus.FOUND);
 	}
 	
 	@GetMapping("/id/{id}")
-	public ResponseEntity<CourseDto> findDescriptionFromName(@PathVariable Integer id) {
+	public ResponseEntity<CourseDto> findDescriptionFromName(@PathVariable Integer id) throws ObjNotFoundException {
 		CourseDto courseDto = courseService.findById(id);
 		return new ResponseEntity<>(courseDto, HttpStatus.FOUND);
 	}
 	
 	@GetMapping("/all")
-	public ResponseEntity<List<CourseDto>> findAll() {
+	public ResponseEntity<List<CourseDto>> findAll() throws EmptyListException {
 		List<CourseDto> listDto =  courseService.findAll();
 		return new ResponseEntity<>(listDto, HttpStatus.FOUND);
 	}
 	
 	@PostMapping("/new")
-	public ResponseEntity<CourseDto> saveNewCourse(@RequestBody CourseDto dto) {
+	public ResponseEntity<CourseDto> saveNewCourse(@RequestBody CourseDto dto) throws InvalidOperationException {
 		CourseDto courseDto = courseService.save(dto);
 		return new ResponseEntity<>(courseDto, HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/update")
-	public ResponseEntity<CourseDto> updateCourseDescription(@RequestBody CourseDto dto) {
+	public ResponseEntity<CourseDto> updateCourseDescription(@RequestBody CourseDto dto) throws InvalidOperationException, ObjNotFoundException {
 		CourseDto courseDto = courseService.update(dto);
 		return new ResponseEntity<>(courseDto, HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/delete")
-	public ResponseEntity<CourseDto> delete(@RequestBody CourseDto dto) {
+	public ResponseEntity<CourseDto> delete(@RequestBody CourseDto dto) throws ObjNotFoundException {
 		CourseDto courseDto = courseService.delete(dto);
 		return new ResponseEntity<>(courseDto, HttpStatus.OK);
 	}
